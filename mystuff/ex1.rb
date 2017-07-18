8d78c22c280099134afe67d7f030e3271abd00a2
WESTKINz <westkinz@hotmail.co.uk>
2017-07-18 16:05:26 +0100

    wChanges to be committed:

---
+++ mystuff/ex1.rb
@@ -0,0 +1,8 @@
+#puts "hello World!"
+#puts "hello Again"
+#puts "I like typing this."
+#puts "this is fun."
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+
+puts 'I "said" do not touch this.'
---
+++ mystuff/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:" #it will print I will now count my chickens:
+
+puts "Hens #{25 + 30 / 6}"  # it will dvide 30/6 and then add it by 25 after the calculation it will print Hens 30
+puts "Roosters #{100 - 25 * 3 % 4}" # By applying the BODMAS rule it should calculate like this 100 - ((25*3) modulo 4) = 97 and then print Roosters 97
+
+puts "Now I will count the eggs:" # it will print Now I will count the eggs:
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # following the BODMAS rule it will divide first and then do the percentage so it should be like this (3 + 2 + 1 - 5 + {4 % 2 - [1/4]} + 6) = 6.75 and rounded it up which is 7. It will print 7
+
+puts "Is it true that 3 + 2 < 5 - 7?" # it will print "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 # it will add 3 + 2 = 5 and substract 5 - 7 = -2. It will compare 5 agaist -2 if the 5 is smaller than -2 becasue we have < statement, which means smaller than. This should print false.
+
+puts "What is 3 + 2? #{3 + 2}" # 3 + 2 = 5 and it will print What is 3 + 2? 5
+puts "What is 5 - 7? #{5 - 7}" # 5 - 7 = -2 and it will print What is 5 - 7? -2
+
+puts "Oh, that's why it's false." # it will print Oh, that's why it's false.
+
+puts "How about some more." # it will print "How about some more."
+
+puts "Is it greater? #{5 > -2}" # we have a statement saying is 5 greater than -2 which it is so it will print Is it greater? true
+puts "Is it greater or equal? #{5 >= -2}" # we have a statement saying is 5 greater or equal than -2. it is not equal however it is greater so it meets one statement. It will print Is it greater or equal? true
+puts "Is it less or equal? #{5 <= -2}" # we have a statement saying is 5 less than or equal than -2. the value is neither eqaul or less  so it will print Is it less or equal? False
